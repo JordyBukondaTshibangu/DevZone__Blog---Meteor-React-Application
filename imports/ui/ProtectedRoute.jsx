bbcc0e977eb8bb0a6e3ef9bc94711120b998f059
Jordy Tshibangu <jordytshibss@gmail.com>
2021-02-15 15:40:19 +0200

    redirecting to signup

--- imports/ui/ProtectedRoute.jsx
+++ imports/ui/ProtectedRoute.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 import {Redirect} from 'react-router-dom';
 
 const ProtectedRoute = props => {
 
+    const history = useHistory();
     const dev = JSON.parse(localStorage.getItem('dev'));
 
+    if (! dev) 
+        history.push('/sign-up');
+    
 
     const Component = props.component;
     const isAuthenticated = dev.email;
+
 
     return isAuthenticated ? <Component propsData={props} dev={dev}/> : <Redirect to={{pathname : "/sign-in"}} />
 }
