bb8308cf5700bdfe02217c81b71f23d33518493f
Jordy Tshibangu <jordytshibss@gmail.com>
2021-02-24 02:54:12 +0200

    search functionality in posts

--- imports/ui/pages/posts/AllPost/AllPostsPage.jsx
+++ imports/ui/pages/posts/AllPost/AllPostsPage.jsx
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { FaSearch } from 'react-icons/fa';
-import './AllPostPage.css';
 import { PostsCollection } from "../../../../db/posts/collection";
+import LoadingSpinner from '../../../feedback/LoadingSpinner'
 import CategorySideBar from '../../../components/categorySideBar/CategorySideBar.jsx'
 import AllPosts from '../../../containers/allPosts/AllPosts.jsx';
+import './AllPostPage.css';
 ;
 
 const PostsPage = ({posts}) => {
-    
+
+    const [ search, setSearch ] = useState('');
+    const [ allPosts, setAllPosts ] = useState([]);
+
+    useEffect(() => {
+        setAllPosts(posts)
+    })
+
+    const handleSearch = event => {
+
+        setSearch(event.target.value);
+
+        const postsResults = allPosts.filter(post => post.title.toLowerCase().includes(search.toLowerCase()));
+        setAllPosts(postsResults)
+    }
     return (
        <div className="posts-container">
             <div className="posts-container-header">
                 <h3> All posts and Features content  </h3>
                 <div className="search-bar">
-                    <input type="text" placeholder="Search Post"/>
+                    <input type="text" placeholder="Search Post" value={search} onChange={event => handleSearch(event)}/>
                     <div className="fa-icon-search">
                         <FaSearch />
                     </div>
@@ -25,7 +40,9 @@ const PostsPage = ({posts}) => {
                     <CategorySideBar />
                 </div>
                 <div>
-                    <AllPosts posts={posts}/>
+                    {
+                        allPosts.length < 1 ? <LoadingSpinner /> : <AllPosts posts={allPosts}/>
+                    }
                 </div>
             </div>
        </div>
